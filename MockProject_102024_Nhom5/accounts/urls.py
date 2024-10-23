04037ddd9c463c9059912240f19e61f16ab5e9af
HoangTran1834 <21115053120302@sv.ute.udn.vn>
2024-10-24 01:28:23 +0700

    change similiar dev

--- MockProject_102024_Nhom5/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-# account/urls.py
-from django.urls import path
-from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-urlpatterns = [
-    path('login', views.login_view, name='api_login'),
-    # path('get_token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-]
