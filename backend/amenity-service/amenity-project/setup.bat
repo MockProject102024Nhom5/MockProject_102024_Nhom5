80e43118eba789a07c6b6d9801aebdb913c518a9
HoangTran1834 <21115053120302@sv.ute.udn.vn>
2024-10-28 08:41:43 +0700

    add amenity-service

--- backend/amenity-service/amenity-project/setup.bat
+++ backend/amenity-service/amenity-project/setup.bat
@@ -1,11 +1,9 @@
 @echo off
 REM Set environment variables for SQL Server connection
 set SPRING_DATASOURCE_URL=jdbc:sqlserver://localhost:1433;databaseName=MockProject_102024_Nhom5;encrypt=false;trustServerCertificate=true
-set SPRING_DATASOURCE_USERNAME=sa
-set SPRING_DATASOURCE_PASSWORD=123
 
 REM Optional: Set Java Home if needed
-set JAVA_HOME=C:\Program Files\Java\jdk-17
+set JAVA_HOME=C:\Program Files\Java\jdk-18.0.2.1
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 REM Run Maven commands
